1dc3b9e6d2ef31bbb4bd3f9f5c9c8c113e0a616c
Alex Yaro <alex.salih4747@googlemail.com>
2020-04-23 20:17:36 +0200

    auto dump mit datum und zu den bestimmten uhrzeiten

--- venv/Files/Main.py
+++ venv/Files/Main.py
@@ -3,20 +3,19 @@
 import os
 import connect
 import mysql
-import subprocess  #
-import time
+import subprocess
+import datetime
 
 
-# dumpdatabase = subprocess.run(["mysqldump", "--socket=/opt/lampp/var/mysql/mysql.sock", "-u", "root", "-p123"]["baufuchs", ">", "~/PycharmProjects/Datensicherung/Dumps/sicherung.sql"])
-# dumpdatabase
+def dump():
+    subprocess.run(
+        'mysqldump --socket=/opt/lampp/var/mysql/mysql.sock -u root -p123 baufuchs > '
+        '~/PycharmProjects/Datensicherung/Dumps/baufuchs_db_$(date "+%b_%d_%Y_%H_%M").sql',
+        shell=True)
 
-def gettimestamp():
-    timestamp = time.strftime('%Y-%m-%d-%I:%M')
-    return timestamp
 
+now = datetime.datetime.now()
 
-dump = subprocess.run(
-    'mysqldump --socket=/opt/lampp/var/mysql/mysql.sock -u root -p123 baufuchs > '
-    '~/PycharmProjects/Datensicherung/Dumps/baufuchs_db_$(date "+%b_%d_%Y_%H_%M_%S").sql',
-    shell=True)
-
+while True:
+    if now.hour == "20" or now.hour == "12":
+        dump()
