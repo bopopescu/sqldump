a58a96060ac26e53106ff9acc16f1d3ea56e55e5
Alex Yaro <alex.salih4747@googlemail.com>
2020-05-19 17:54:41 +0200

    File-control fertiggestellt, funktioniert nun.

--- venv/Files/file-control.py
+++ venv/Files/file-control.py
@@ -11,20 +11,29 @@ archivpath = "/home/alex/PycharmProjects/Datensicherung/Archiv/"
 twoweeks = arrow.now().shift(hours=+5).shift(days=-14)
 fourweeks = arrow.now().shift(hours=+5).shift(days=-14)
 
-for item in Path(dumppath).glob('*.gz'):
-    if item.is_file():
-        print(str(item.absolute()))
-        itemTime = arrow.get(item.stat().st_mtime)
-        if itemTime < twoweeks:
-            shutil.move(item, archivpath)
-            pass
+
+def archiv():
+    for item in Path(dumppath).glob('*.gz'):
+        if item.is_file():
+            itemTime = arrow.get(item.stat().st_mtime)
+            if itemTime > twoweeks:
+                shutil.move(str(item.absolute()), archivpath)
+                print(str(item.absolute()), "was archived")
+                pass
+            if itemTime < twoweeks:
+                print(str(item.absolute()), "not old enough to archive...")
 
 
-for item in Path(dumppath).glob('*12_00_00.sql.gz'):
-    if item.is_file():
-        print(item.absolute())
-        itemTime = arrow.get(item.stat().st_mtime)
-        if itemTime > fourweeks:
-            os.remove(item.absolute(), dir_fd=None)
-        else:
-            print("error")
+def delete():
+    for item in Path(archivpath).glob('*'):  # eigentlich .glob('*12_00_00.sql.gz')
+        if item.is_file():
+            itemTime = arrow.get(item.stat().st_mtime)
+            if itemTime > fourweeks:
+                os.remove(str(item.absolute()), dir_fd=None)
+                print(item.absolute(), "was deleted...")
+            if itemTime < fourweeks:
+                print(item.absolute(), "not old enough to delete...")
+
+
+archiv()
+delete()
